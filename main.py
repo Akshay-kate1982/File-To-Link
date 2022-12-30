07ceb6c4092642d01debe007a4aee4f1400b6e00
Akshay-kate1982 <121658777+Akshay-kate1982@users.noreply.github.com>
2022-12-30 17:01:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import logging
 import importlib
 from pathlib import Path
 from pyrogram import idle
-from AKSHAY.bot import StreamBot
-from AKSHAY.vars import Var
+from .bot import StreamBot
+from .vars import Var
 from aiohttp import web
-from AKSHAY.server import web_server
-from AKSHAY.utils.keepalive import ping_server
+from .server import web_server
+from .utils.keepalive import ping_server
 from AKSHAY.bot.clients import initialize_clients
 
 logging.basicConfig(
